f59220f540dec9bd4d673e421c25033bac3ad06c
Fatima Rizvi <fatimarzv23@gmail.com>
2020-09-18 10:39:19 -0500

    Extreme barebones of first page running with link to the currently unbuilt form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+import { Route, Link, Switch } from 'react-router-dom'
+import PizzaForm from './Form'
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <h2>Click the button below to get started!</h2>
+      <nav>
+        <div className = 'pizza-link'>
+          <Link to='/pizza-form' className = 'buildPizzaBtn'>Build My Pizza</Link>
+        </div>
+      </nav>
+      <Switch>
+        <Route path = '/pizza-form'>
+          <PizzaForm />
+        </Route>
+      </Switch>
     </>
   );
 };
---
+++ src/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PizzaForm () {
+    const farewell = "bye"
+    return(
+        <div>
+            <h1>Hi</h1>
+            <h4>{farewell}</h4>
+        </div>
+    )
+}
